14bb4871dcf458f98f1779df856a7049206fafb0
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-10-06 20:13:45 +0000

    -Mise en place de la page d'acceuil -Integration des produits -Mise Layout

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Head from 'next/head'
 
-const Layout = () => {
+import { Navbar, Footer} from '../components'
+
+
+const Layout = ( { children }) => {
   return (
-    <div>
-      
+    <div className='layout'>
+      <Head>
+        <title>DHJ Store</title>
+      </Head>
+      <header>
+          <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
     </div>
   )
 }
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -8,7 +8,7 @@ const Home = ({products, bannerData}) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
-      {console.log(bannerData)}
+      
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -16,10 +16,10 @@ const Home = ({products, bannerData}) => {
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
 
-      <FooterBanner/>
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </div>
   );
 
