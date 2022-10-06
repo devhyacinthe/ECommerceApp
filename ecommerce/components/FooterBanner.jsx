a2e4bca5e423c96a9e19babd0889886ff8746d1d
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-10-06 19:26:03 +0000

    Connecter sanity a nextJs et mise en place du banner

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FooterBanner = () => {
   return (
     <div>
-      HEllo
+      FooterBanner
     </div>
   )
 }
--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Link } from 'next/link'
+import Link from 'next/link'
+import { urlFor } from '../lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
+
   return (
     <div className='hero-banner-container'>
-      <div>
-        <p className='beats-solo'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="shoes" className='hero-banner-image' />
-
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
-          </Link>
-          <div className='desc'>
-              <h5>Description</h5>
-              <p>DESCRIPTION</p>
+          <p className='beats-solo'>{heroBanner.smallText}</p>
+          <h3>{heroBanner.midText}</h3>
+          <h1>{heroBanner.largeText1}</h1>
+          <img src={urlFor(heroBanner.image)} alt="shoes" className='hero-banner-image'/>
+          <div>
+              <Link href={`/product/${heroBanner.product}`}>
+                  <button type="button">
+                      {heroBanner.buttonText}
+                  </button>
+              </Link>
+              <div className='desc'>
+                <h5>Description</h5>
+                <p>{heroBanner.desc}</p>
+              </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
