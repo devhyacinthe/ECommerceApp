c21cfc5ae8739a331f592bc307db5a923278b883
devhyacinthe <yacinthedalmeida2002@gmail.com>
2022-10-07 20:41:15 +0000

    Mise en place de la logique de l'application La gestion du panier et mise en place du context globale de l'application

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
+import { Toaster } from 'react-hot-toast'
+
 import { Layout } from '../components'
 import '../styles/globals.css'
+import { StateContext } from '../context/StateContext'
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <StateContext>
+      <Layout>
+        <Toaster/>
         <Component {...pageProps} />
-    </Layout>
+      </Layout>
+    </StateContext>
+    
   )
 }
 
